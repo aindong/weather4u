89847d30b925c78c0204ed82f35c2fa40a3b0887
Alleo Indong <freedom_war550@live.com.ph>
2015-03-05 22:28:38 +0800

    Updated the facade to test

--- Acme/Weather/WeatherClass.php
+++ Acme/Weather/WeatherClass.php
@@ -2,9 +2,44 @@
 
 class WeatherClass {
 
-    public function sayHello()
+    const URL = 'api.openweathermap.org/data/2.5/weather';
+    const APPID = 'd76d1f72a4a7f43d9e654b3bdc58b929';
+
+    private $requestUrl = '';
+
+    public function getWeatherByCity($city)
     {
-        echo 'Hello';
+        // Build the request url
+        $this->requestUrl = URL . '?q=' . $city;
+
+        // Throw the request
+        $request = $this->request($this->requestUrl);
+
+        return json_encode($request);
     }
 
+    /**
+     * Start the request to the api source
+     *
+     * @param $url
+     * @return mixed
+     */
+    private function request($url)
+    {
+        // Initialize curl
+        $curl = curl_init();
+
+        // Curl options
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1, // Return the result as a string
+            CURLOPT_URL => $url
+        ]);
+
+        // Execute the request
+        $result = curl_exec($curl);
+        // Close the connection
+        curl_close($curl);
+
+        return $result;
+    }
 }
