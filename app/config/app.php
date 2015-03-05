c6ee76c6a0644ea11756586c44580d643f582774
Alleo Indong <freedom_war550@live.com.ph>
2015-03-05 21:36:08 +0800

    Added debugbar for laravel, for debugging purposes, added a route for homepage, a homecontroller@index function for homepage and the default layout for the page

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -121,7 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-
+        'Barryvdh\Debugbar\ServiceProvider',
 	),
 
 	/*
@@ -189,6 +189,7 @@ return array(
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
 
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',
 	),
 
 );
